0ab9c1705646de7faf584d6c9c5b771d81f32bad
Garth Whitaker <garth.whitaker@gmail.com>
2018-04-21 14:18:23 +0100

    Interactive package updates to latest versions

--- cli.js
+++ cli.js
@@ -1,32 +1,99 @@
 #!/usr/bin/env node
+const program = require("commander");
+const inquirer = require("inquirer");
+const main = require("./index");
+const npm = require("npm");
+const chalk = require('chalk');
 
-const program = require('commander');
-const main = require('./index');
+program.version("0.0.1").description("Automated package checker tool");
 
 program
-  .version('0.0.1')
-  .description('Automated package checker tool');
-
-program
-  .command('check <package-name>')
-  .description('Check a specific package')
+  .command("check <package-name>")
+  .description("Check a specific package")
   .action(packageName => {
     console.log(`checking a specific package: ${packageName}`);
     main.checkPackage(packageName);
   });
 
 program
-  .command('*')
-  .description('Unsupported command')
+  .command("*")
+  .description("Unsupported command")
   .action(() => {
-    console.warn('The command you run is not supported. See help details below to verify your command.');
+    console.warn(
+      "The command you run is not supported. See help details below to verify your command."
+    );
     program.outputHelp();
     process.exit();
   });
 
+program
+  .option("--i")
+  .description("interactive mode")
+  .parse(process.argv);
+
 if (process.argv.slice(2).length === 0) {
-  console.log('No specific package specified - checking all dependencies in package.json');
-  main.checkAllDeps();
+  console.log(
+    "No specific package specified - checking all dependencies in package.json"
+  );
+  main
+    .checkAllDeps()
+    .then(result => {
+      const availableUpdates = result.filter(
+        item => Object.keys(item).length > 0
+      );
+
+      const packages = availableUpdates.reduce((accumulator, current) => {
+        accumulator[current.name] = current.packageVersion;
+        return accumulator;
+      }, {});
+
+     if(!availableUpdates.length)
+     {
+       console.log(chalk.green('All packages are up to date.'));
+       return; 
+     }
+
+      inquirer
+        .prompt([
+          {
+            type: "checkbox",
+            message: `Select from the available updates  or press (Ctrl + C) to exit.\n`,
+            name: "update",
+            choices: [...availableUpdates]
+          }
+        ])
+
+        .then(answer => {
+          if (answer.update !== "Cancel") {
+        console.log(answer);
+
+            for(let i=0; i < answer.update.length; i++) {
+            npm.load(function(err) {
+          
+              npm.commands.install([`${answer.update[i]}@${packages[answer.update[i]]}`], function (er, data) {
+                if (er) return console.error(er);
+                console.log(data);
+                // command succeeded, and data might have some info
+              })
+
+              npm.on('log', function(message) {
+                // log installation progress
+                console.log(message);
+              });
+            });
+          }
+
+          } else {
+            console.log("Exit");
+          }
+      
+        });
+    })
+    .then()
+    .catch(console.error);
 }
 
+if (program.i) {
+  console.log("interactive mode");
+}
 program.parse(process.argv);
--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 const axios = require("axios");
 const fs = require("fs");
-const chalk = require('chalk');
-const config = require('./config');
+const chalk = require("chalk");
+const config = require("./config");
+const semver = require('semver');
 
 const checkAllDeps = () => {
-  const result = {};
-  
-  fs.readFile("package.json", "utf8", (err, data) => {
-    if (err) throw err;
+  const result = [];
 
-    const { dependencies } = JSON.parse(data);
+  const data = fs.readFileSync("package.json", "utf8");
+  const { dependencies } = JSON.parse(data);
 
-    for (let packageName in dependencies) {
-      checkPackage(packageName);
-    }
-  });
+  for (let packageName in dependencies) {
+    const version = semver.valid(semver.coerce(dependencies[packageName]));
+    result.push(checkPackage(packageName, version));
+  }
+
+  return Promise.all(result);
 };
 
-const checkPackage = packageName =>
-  axios
-    .get(`${config.api}/${packageName}`)
+const checkPackage = (packageName, version) => {
+  const url = `${config.api}/${packageName}`;
+
+  return axios
+    .get(`${url}`)
     .then(response => {
       const {
         data: {
-          collected: {
-            metadata
-          }
+          collected: { metadata }
         }
       } = response;
-      console.log(chalk.blue(`${packageName}: ${metadata.version}`));
+      const packageInfo = { name: metadata.name, packageVersion: metadata.version };
+      return semver.gt(metadata.version,version)
+        ? packageInfo
+        : {};
     })
     .catch(error => {
       console.error(error.message);
     });
+};
 
 module.exports = {
   checkAllDeps,
-  checkPackage,
+  checkPackage
 };
